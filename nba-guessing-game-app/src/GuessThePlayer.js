c1dbc490aa128a7fa41bf64b721b061e3a5f1f8f
tswartzendruber <travswartzenduber@gmail.com>
2024-01-18 17:30:18 -0600

    Player data gets displayed in a table when selected

--- nba-guessing-game-app/src/GuessThePlayer.js
+++ nba-guessing-game-app/src/GuessThePlayer.js
@@ -9,6 +9,8 @@ const GuessThePlayer = () => {
   const [playerSelections, setPlayerSelections] = useState(null);   // playerSelections is the names of all players from that season
   const [filterText, setFilterText] = useState("");   // filterText is the string typed into the text entry box
   const [filterBy, setFilterBy] = useState("firstName");
+  const headers = ["Player", "Tm", "Pos", "Age", "G", "MP", "PTS", "TRB", "AST", "STL", "BLK", "TOV", "FG", "FGA", "FGpct", "Threes", "ThreesA", "Threepct", "FT", "FTA", "FTpct"];
+  const [numGuesses, setNumGuesses] = useState(1);
 
   var filteredOptions = playerSelections ? Array.from(playerSelections.options).filter(
     (option) => option.text.replace(/\s/g, "").toLowerCase().startsWith(filterText.replace(/\s/g, "").toLowerCase())): [];
@@ -54,6 +56,9 @@ const GuessThePlayer = () => {
       filteredOptions = playerSelections ? Array.from(playerSelections.options).filter(
         (option) => option.text.replace(/\s/g, "").toLowerCase().startsWith(filterText.replace(/\s/g, "").toLowerCase())): [];
     };
+
+    filteredOptions = filteredOptions.slice(0, 6);
+
     if (filteredOptions.length === 0 || filterText.length === 0) {
       document.getElementById("playerNames").style.display = "none";
     } else {
@@ -69,6 +74,31 @@ const GuessThePlayer = () => {
       setFilterBy("firstName");
       document.getElementById("switchLabel").innerHTML = "Sorting by First Name";
     }
+  };
+  
+  const selectPlayer = (e) => {
+    const selectedPlayerIndex = e.target.value;
+    const selectedPlayer = data[selectedPlayerIndex];
+
+    document.getElementById("guessingTextEntryBox").value = "";
+    setFilterText("");
+  
+    const newRow = document.createElement("tr");
+  
+    headers.forEach((header) => {
+      const cell = document.createElement("td");
+      cell.textContent = selectedPlayer[header];
+      newRow.appendChild(cell);
+    });
+  
+    const tableBody = document.querySelector("#statsTable tbody");
+    tableBody.appendChild(newRow);
+
+    if (numGuesses === 1) {
+      document.getElementById("statsTable").style.display = "block";
+    }
+
+    setNumGuesses((previousNumGuesses) => previousNumGuesses + 1);
   };
 
   return (
@@ -98,26 +128,61 @@ const GuessThePlayer = () => {
 
         <input 
           className="guessingTextEntry"
+          id="guessingTextEntryBox"
           type="text" 
           autoComplete="off" 
           aria-autocomplete="list" 
           aria-activedescendant="" 
           aria-controls="autosuggest-autosuggest__results" 
-          placeholder="Guess 1 of 8"
+          placeholder={"Guess " + numGuesses + " of 8"}
           onChange={(e) => setFilterText(e.target.value)}
         />
 
         <br></br>
 
         {playerSelections ? (
-          <select className="guessingTextEntry" id="playerNames">
+          <ul id="playerNames">
             {filteredOptions.map((option, index) => (
-              <option key={index} value={option.value}>
+              <li key={index} value={option.value} id="playerName" onClick={selectPlayer}>
                 {option.text}
-              </option>
+              </li>
             ))}
-          </select>
-        ) : <select className="guessingTextEntry" id="playerNames"></select>}
+          </ul>
+        ) : <ul className="guessingTextEntry" id="playerNames"></ul>}
+
+        {data.length ? (
+          <div id="statsTableDiv">
+            <table id="statsTable" style={{display: "none"}}>
+              <thead>
+                <tr>
+                  <th id="namesColumn">PLAYER NAME</th>
+                  <th className="generalStats">TEAM</th>
+                  <th className="generalStats">POS</th>
+                  <th className="generalStats">AGE</th>
+                  <th className="generalStats">GP</th>
+                  <th className="generalStats">MIN</th>
+                  <th className="generalStats">PTS</th>
+                  <th className="generalStats">REB</th>
+                  <th className="generalStats">AST</th>
+                  <th className="generalStats">STL</th>
+                  <th className="generalStats">BLK</th>
+                  <th className="generalStats">TOV</th>
+                  <th className="generalStats">FGM</th>
+                  <th className="generalStats">FGA</th>
+                  <th className="generalStats">FG%</th>
+                  <th className="generalStats">3PM</th>
+                  <th className="generalStats">3PA</th>
+                  <th className="generalStats">3P%</th>
+                  <th className="generalStats">FTM</th>
+                  <th className="generalStats">FTA</th>
+                  <th className="generalStats">FT%</th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+          </div>
+
+          ) : null}
 
       </div>
     </>
--- nba-guessing-game-app/src/Home.js
+++ nba-guessing-game-app/src/Home.js
@@ -1,3 +1,36 @@
+/*
+Classic Game Mode
+-----------------
+
+Bench-Warmer Difficulty:
+  * Number of Guesses: 10
+  * Season(s): 2023-24
+  * Player Pool: minimum 30 minutes per game
+
+All-Star Difficulty:
+  * Number of Guesses: 8
+  * Season(s): 2023-24
+  * Player Pool: minimum 0.1 seconds of recorded game action
+
+Hall of Fame Difficulty:
+  * Number of Guesses: 5
+  * Season(s): ####-## to 2023-24
+  * Player Pool: minimum 30 minutes per game
+
+Greatest of All Time Difficulty:
+  * Number of Guesses: 1
+  * Season(s): ####-## to 2023-24
+  * Player Pool: minimum 0.1 seconds of recorded game action
+*/
+
+/*
+Poole Party (Custom Game Mode)
+------------------------------
+  * Number of Guesses: User can Choose (Range: 0, 100)
+  * Season(s): User can Choose (All seasons (career averages for stats)  OR  any single season (season averages for stats))
+  * Player Pool: User can Choose by Stat(s) (e.g. minimum 11.3 points per game and maximum 0.8 blocks per game)
+*/
+
 const Home = () => {
     return <h1>Home</h1>;
   };
