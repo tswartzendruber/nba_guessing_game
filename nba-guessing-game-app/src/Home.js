5f7cb3c2a3ac75ae3ee3b3c1d6d02d1a5c840a6a
tswartzendruber <travswartzenduber@gmail.com>
2024-01-22 13:38:28 -0600

    Highlight the select column to sort in the stats table and updated logic for table

--- nba-guessing-game-app/src/Home.js
+++ nba-guessing-game-app/src/Home.js
@@ -1,3 +1,4 @@
+/*
 import './App.css';
 import React, { useState, useEffect } from "react";
 import Papa from "papaparse";
@@ -177,3 +178,4 @@ const Home = () => {
 }
   
 export default Home;
+*/
--- nba-guessing-game-app/src/Stats.js
+++ nba-guessing-game-app/src/Stats.js
@@ -2,75 +2,17 @@ import './App.css';
 import React, { useState, useEffect } from "react";
 import Papa from "papaparse";
 
-function sortTable(n) {
-  var table, rows, switching, i, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("statsTable");
-  switching = true;
-
-  // Get the current sorting direction from the table's data attribute
-  dir = table.getAttribute("data-sort-dir") === "asc" ? "desc" : "asc";
-  table.setAttribute("data-sort-dir", dir);
-
-  rows = Array.from(table.rows).slice(1); // Exclude the header row
-
-  while (switching) {
-    switching = false;
-    for (i = 0; i < rows.length - 1; i++) {
-      shouldSwitch = false;
-      const x = rows[i].getElementsByTagName("TD")[n].innerHTML;
-      const y = rows[i + 1].getElementsByTagName("TD")[n].innerHTML;
-
-      let compareResult;
-      if (n > 2) {
-        compareResult = parseFloat(x) - parseFloat(y);
-      } else if (n === 0) {
-        const xName = x.split(" ");
-        const yName = y.split(" ");
-        compareResult = xName[1] === yName[1] ? xName[0].localeCompare(yName[0]) : xName[1].localeCompare(yName[1]);
-      } else if (n === 1) {
-        compareResult = x.toLowerCase().localeCompare(y.toLowerCase());
-      } else if (n === 2) {
-        const getPositionValue = position => {
-          const positionsMap = { 
-            "PG": 100, "PG-SG": 110, "PG-SF": 120, "PG-PF": 130, "PG-C": 140,
-            "SG": 200, "SG-PG": 175, "SG-SF": 210, "SG-PF": 220, "SG-C": 230,
-            "SF": 300, "SF-PG": 260, "SF-SG": 270, "SF-PF": 310, "SF-C": 320,
-            "PF": 400, "PF-PG": 330, "PF-SG": 340, "PF-SF": 350, "PF-C": 410,
-            "C": 500, "C-PG": 420, "C-SG": 430, "C-SF": 440, "C-PF": 450,
-          };
-          return positionsMap[position];
-        };
-
-        compareResult = getPositionValue(x) - getPositionValue(y);
-      }
-
-      if ((dir === "asc" && compareResult > 0) || (dir === "desc" && compareResult < 0)) {
-        shouldSwitch = true;
-        const temp = rows[i];
-        rows[i] = rows[i + 1];
-        rows[i + 1] = temp;
-        switching = true;
-        switchcount++;
-      }
-    }
-  }
-
-  // Update the table with the sorted rows
-  const tbody = table.tBodies[0];
-  tbody.innerHTML = "";
-  for (const row of rows) {
-    tbody.appendChild(row);
-  }
-}
-
 const Stats = () => {
 
   const [data, setData] = useState([]);
   const [season, setSeason] = useState("2023-24");
+  const [loading, setLoading] = useState(true);
+
+  const [sortedColumn, setSortedColumn] = useState(null);
 
   useEffect(() => {
-    // Reading data from csv file
     const fetchData = async () => {
+      try {
       const response = await fetch("./NBA_Stats_by_Season/" + season + ".csv");
       const reader = response.body.getReader();
       const result = await reader.read();
@@ -82,7 +24,6 @@ const Stats = () => {
       }).data;
 
       const uniquePlayers = Array.from(new Set(parsedData.map(row => row.Player)));
-
       const lastTeam = {};
 
       parsedData.forEach(row => {
@@ -96,9 +37,119 @@ const Stats = () => {
       });
       
       setData(uniqueData);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
     };
     fetchData();
   }, [season]);
+
+  function sortTable(n) {
+    setSortedColumn(n);
+    highlightColumn(n);
+
+    function highlightColumn(columnIndex) {
+      const table = document.getElementById("statsTable");
+      const tbody = table.tBodies[0];
+      const rows = Array.from(tbody.getElementsByTagName("tr"));
+      const clickedColumn = document.getElementById(`statsTableHead`).getElementsByTagName("th")[columnIndex];
+    
+      const headerCells = document.getElementById("statsTableHead").getElementsByTagName("th");
+      for (const headerCell of headerCells) {
+        headerCell.style.backgroundColor = "white";
+      }
+    
+      for (const row of rows) {
+        const cells = Array.from(row.getElementsByTagName("td"));
+        for (const cell of cells) {
+          cell.style.backgroundColor = "white";
+        }
+    
+        const selectedCell = cells[columnIndex];
+        selectedCell.style.backgroundColor = "rgb(243, 234, 56)";
+      }
+    
+      clickedColumn.style.backgroundColor = "rgb(243, 234, 56)";
+    }
+
+    //var table, rows, switching, i, shouldSwitch, dir, switchcount = 0;
+    var table, rows, switching, i, dir;
+    table = document.getElementById("statsTable");
+    switching = true;
+
+    if (sortedColumn !== n) {
+      dir = "desc"; // Set initial sorting direction to "desc" if the column is not already sorted
+    } else {
+      dir = table.getAttribute("data-sort-dir") === "desc" ? "asc" : "desc";
+    }
+    table.setAttribute("data-sort-dir", dir);
+  
+    rows = Array.from(table.rows).slice(1);
+  
+    while (switching) {
+      switching = false;
+      for (i = 0; i < rows.length - 1; i++) {
+        //shouldSwitch = false;
+        const x = rows[i].getElementsByTagName("TD")[n].innerHTML;
+        const y = rows[i + 1].getElementsByTagName("TD")[n].innerHTML;
+  
+        let compareResult;
+        if (n > 2) {
+          compareResult = parseFloat(x) - parseFloat(y);
+        } else if (n === 0) {
+          const xName = x.split(" ");
+          const yName = y.split(" ");
+          compareResult = xName[1] === yName[1] ? xName[0].localeCompare(yName[0]) : xName[1].localeCompare(yName[1]);
+        } else if (n === 1) {
+          compareResult = x.toLowerCase().localeCompare(y.toLowerCase());
+        } else if (n === 2) {
+          const getPositionValue = position => {
+            const positionsMap = { 
+              "PG": 100, "PG-SG": 110, "PG-SF": 120, "PG-PF": 130, "PG-C": 140,
+              "SG": 200, "SG-PG": 175, "SG-SF": 210, "SG-PF": 220, "SG-C": 230,
+              "SF": 300, "SF-PG": 260, "SF-SG": 270, "SF-PF": 310, "SF-C": 320,
+              "PF": 400, "PF-PG": 330, "PF-SG": 340, "PF-SF": 350, "PF-C": 410,
+              "C": 500, "C-PG": 420, "C-SG": 430, "C-SF": 440, "C-PF": 450,
+            };
+            return positionsMap[position];
+          };
+  
+          compareResult = getPositionValue(x) - getPositionValue(y);
+        }
+  
+        if ((dir === "asc" && compareResult > 0) || (dir === "desc" && compareResult < 0)) {
+          //shouldSwitch = true;
+          const temp = rows[i];
+          rows[i] = rows[i + 1];
+          rows[i + 1] = temp;
+          switching = true;
+          //switchcount++;
+        }
+      }
+    };
+  
+    if (dir === "asc") {
+      document.getElementById(`statsColumnArrow${n}`).innerHTML = "↑";
+      document.getElementById(`statsColumnArrow${n}`).style.display = "block";
+    } else if (dir ==="desc") {
+      document.getElementById(`statsColumnArrow${n}`).innerHTML = "↓";
+      document.getElementById(`statsColumnArrow${n}`).style.display = "block";
+    };
+  
+    for (let i = 0; i < 21; i++) {
+      if (i !== n) {
+        document.getElementById(`statsColumnArrow${i}`).style.display = "none";
+      };
+    };
+
+
+    const tbody = table.tBodies[0];
+    tbody.innerHTML = "";
+    for (const row of rows) {
+      tbody.appendChild(row);
+    }
+  };
 
   return (
     <div className="App">
@@ -113,33 +164,35 @@ const Stats = () => {
         </select>
       </div>
 
-      {data.length ? (
+      {loading ? (
+        <p>Loading...</p>) 
+      : data.length ? (
         <>
         <div className="scroll">
         <table id="statsTable">
           <thead>
             <tr id="statsTableHead">
-              <th id="namesColumn" onClick={() => sortTable(0)}>PLAYER NAME</th>
-              <th className="generalStats" onClick={() => sortTable(1)}>TEAM</th>
-              <th className="generalStats" onClick={() => sortTable(2)}>POS</th>
-              <th className="generalStats" onClick={() => sortTable(3)}>AGE</th>
-              <th className="generalStats" onClick={() => sortTable(4)}>GP</th>
-              <th className="generalStats" onClick={() => sortTable(5)}>MIN</th>
-              <th className="generalStats" onClick={() => sortTable(6)}>PTS</th>
-              <th className="generalStats" onClick={() => sortTable(7)}>REB</th>
-              <th className="generalStats" onClick={() => sortTable(8)}>AST</th>
-              <th className="generalStats" onClick={() => sortTable(9)}>STL</th>
-              <th className="generalStats" onClick={() => sortTable(10)}>BLK</th>
-              <th className="generalStats" onClick={() => sortTable(11)}>TOV</th>
-              <th className="generalStats" onClick={() => sortTable(12)}>FGM</th>
-              <th className="generalStats" onClick={() => sortTable(13)}>FGA</th>
-              <th className="generalStats" onClick={() => sortTable(14)}>FG%</th>
-              <th className="generalStats" onClick={() => sortTable(15)}>3PM</th>
-              <th className="generalStats" onClick={() => sortTable(16)}>3PA</th>
-              <th className="generalStats" onClick={() => sortTable(17)}>3P%</th>
-              <th className="generalStats" onClick={() => sortTable(18)}>FTM</th>
-              <th className="generalStats" onClick={() => sortTable(19)}>FTA</th>
-              <th className="generalStats" onClick={() => sortTable(20)}>FT%</th>
+              <th id="namesColumn" onClick={() => sortTable(0)}>PLAYER NAME <p className="statsColumnArrow" id="statsColumnArrow0" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(1)}>TEAM <p className="statsColumnArrow" id="statsColumnArrow1" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(2)}>POS <p className="statsColumnArrow" id="statsColumnArrow2" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(3)}>AGE <p className="statsColumnArrow" id="statsColumnArrow3" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(4)}>GP <p className="statsColumnArrow" id="statsColumnArrow4" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(5)}>MIN <p className="statsColumnArrow" id="statsColumnArrow5" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(6)}>PTS <p className="statsColumnArrow" id="statsColumnArrow6" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(7)}>REB <p className="statsColumnArrow" id="statsColumnArrow7" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(8)}>AST <p className="statsColumnArrow" id="statsColumnArrow8" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(9)}>STL <p className="statsColumnArrow" id="statsColumnArrow9" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(10)}>BLK <p className="statsColumnArrow" id="statsColumnArrow10" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(11)}>TOV <p className="statsColumnArrow" id="statsColumnArrow11" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(12)}>FGM <p className="statsColumnArrow" id="statsColumnArrow12" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(13)}>FGA <p className="statsColumnArrow" id="statsColumnArrow13" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(14)}>FG% <p className="statsColumnArrow" id="statsColumnArrow14" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(15)}>3PM <p className="statsColumnArrow" id="statsColumnArrow15" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(16)}>3PA <p className="statsColumnArrow" id="statsColumnArrow16" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(17)}>3P% <p className="statsColumnArrow" id="statsColumnArrow17" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(18)}>FTM <p className="statsColumnArrow" id="statsColumnArrow18" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(19)}>FTA <p className="statsColumnArrow" id="statsColumnArrow19" style={{display: "none"}}></p></th>
+              <th className="generalStats" onClick={() => sortTable(20)}>FT% <p className="statsColumnArrow" id="statsColumnArrow20" style={{display: "none"}}></p></th>
             </tr>
           </thead>
           <tbody>
