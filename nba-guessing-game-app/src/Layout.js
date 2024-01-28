29663bdf4e9b8a878efc975b3f7456b60ccd7cb5
tswartzendruber <travswartzenduber@gmail.com>
2024-01-28 15:36:34 -0600

    Added CSS to both pages, also fixed an issue with Netlify's routing

--- nba-guessing-game-app/src/Layout.js
+++ nba-guessing-game-app/src/Layout.js
@@ -14,9 +14,13 @@ const Layout = () => {
             <div className="navDropDown">
                 <button className="navDropDownButton" onClick={toggleNavDisplay}><i className="material-symbols-outlined">menu</i></button>
                 <div className={`navDropDownOptions ${navDisplay ? 'open' : 'closed'}`}>
+                    {/*
                     <Link to="/"><p className="navPara">NBA Stats</p></Link>
                     <Link to="/guess-the-player"><p className="navPara">Guess The NBA Player</p></Link>
-                    {/*<Link to="/full-table"><p className="navPara">Full Table</p></Link>*/}
+                    <Link to="/full-table"><p className="navPara">Full Table</p></Link>
+                    */}
+                    <Link to="/"><p className="navPara">NBA Stats</p></Link>
+                    <Link to="/guess-the-player"><p className="navPara">Guess The NBA Player</p></Link>
                 </div>
             </div>
 
